47628d76cae86e734b8474818c5b1d1c1a140ec6
jamesrothmann <35830265+jamesrothmann@users.noreply.github.com>
2022-12-12 21:04:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,13 +152,13 @@ if st.button("Find"):
   results2 = search(query,embeddings)
   st.write("Results:")
 # Open the result.text file in read mode
-   f = open('result.text', 'r')
+  f = open('result.text', 'r')
 
 # Read the contents of the file
-   file_content = f.read()
+  file_content = f.read()
 
 # Close the file
-   f.close()
+  f.close()
 
 # Use the st.text() method to display the contents of the file
-   st.text(file_content)
+  st.text(file_content)
