d952e299a9878755aa29a918833aae5edb9aad42
jamesrothmann <35830265+jamesrothmann@users.noreply.github.com>
2022-12-13 06:02:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 import json
-from bs4 import BeautifulSoup
 from os.path import exists
-from IPython.display import HTML, display
 import numpy as np
 import math
 
@@ -21,57 +18,6 @@ path = "file.json"
 
 
 model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')
-
-
-def part_to_chapter(part):
-    soup = BeautifulSoup(part.get_body_content(), 'html.parser')
-    paragraphs = [para.get_text().strip() for para in soup.find_all('p')]
-    paragraphs = [para for para in paragraphs if len(para) > 0]
-    if len(paragraphs) == 0:
-        return None
-    title = ' '.join([heading.get_text() for heading in soup.find_all('h1')])
-    return {'title': title, 'paras': paragraphs}
-
-min_words_per_para = 150
-max_words_per_para = 500
-
-def format_paras(chapters):
-    for i in range(len(chapters)):
-        for j in range(len(chapters[i]['paras'])):
-            split_para = chapters[i]['paras'][j].split()
-            if len(split_para) > max_words_per_para:
-                chapters[i]['paras'].insert(j + 1, ' '.join(split_para[max_words_per_para:]))
-                chapters[i]['paras'][j] = ' '.join(split_para[:max_words_per_para])
-            k = j
-            while len(chapters[i]['paras'][j].split()) < min_words_per_para and k < len(chapters[i]['paras']) - 1:
-                chapters[i]['paras'][j] += '\n' + chapters[i]['paras'][k + 1]
-                chapters[i]['paras'][k + 1] = ''
-                k += 1            
-
-        chapters[i]['paras'] = [para.strip() for para in chapters[i]['paras'] if len(para.strip()) > 0]
-        if len(chapters[i]['title']) == 0:
-            chapters[i]['title'] = '(Unnamed) Chapter {no}'.format(no=i + 1)
-
-def print_previews(chapters):
-    for (i, chapter) in enumerate(chapters):
-        title = chapter['title']
-        wc = len(' '.join(chapter['paras']).split(' '))
-        paras = len(chapter['paras'])
-        initial = chapter['paras'][0][:20]
-        preview = '{}: {} | wc: {} | paras: {}\n"{}..."\n'.format(i, title, wc, paras, initial)
-        print(preview)
-
-def get_chapters(book_path, print_chapter_previews, first_chapter, last_chapter):
-    book = epub.read_epub(book_path)
-    parts = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-    chapters = [part_to_chapter(part) for part in parts if part_to_chapter(part) is not None]
-    last_chapter = min(last_chapter, len(chapters) - 1)
-    chapters = chapters[first_chapter:last_chapter + 1]
-    format_paras(chapters)
-    if print_chapter_previews:
-        print_previews(chapters)
-    return chapters
-  
   
 def get_embeddings(texts):
     if type(texts) == str:
